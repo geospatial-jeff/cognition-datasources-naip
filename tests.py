81385ac37f991740b1f5e5db764756f4f497cc27
Jeff Albrecht <geospatialjeff@gmail.com>
2019-04-13 15:41:45 -0600

    updating test cases

--- tests.py
+++ tests.py
@@ -1,6 +1,18 @@
+import os
+import sys
+
+db_path = '/home/slingshot/Documents/Cognition/cognition-datasources-naip/spatial-db/lambda_db'
+if db_path not in sys.path:
+  sys.path.append(db_path)
+
+os.environ['S2_LIBRARY_PATH'] = '/home/slingshot/Downloads/s2geometry/build/python'
+os.environ['LAMBDA_DB_PATH'] = '/home/slingshot/Documents/Cognition/cognition-datasources-naip/spatial-db/lambda_db/database.fs'
+
 from datasources import tests
 
 from NAIP import NAIP
+
+
 
 class NAIPTestCases(tests.BaseTestCases):
 
@@ -8,32 +20,33 @@ class NAIPTestCases(tests.BaseTestCases):
 
         self.datasource = NAIP
         self.spatial = {
-                    "type": "Polygon",
-                    "coordinates": [
-                      [
-                        [
-                          -101.28433227539062,
-                          46.813218976041945
-                        ],
-                        [
-                          -100.89431762695312,
-                          46.813218976041945
-                        ],
-                        [
-                          -100.89431762695312,
-                          47.06450941441436
-                        ],
-                        [
-                          -101.28433227539062,
-                          47.06450941441436
-                        ],
-                        [
-                          -101.28433227539062,
-                          46.813218976041945
-                        ]
-                      ]
-                    ]
-                  }
-        self.temporal = ("2017-01-01", "2017-12-31")
+        "type": "Polygon",
+        "coordinates": [
+          [
+            [
+              -119.46533203125,
+              33.26624989076275
+            ],
+            [
+              -116.56494140625001,
+              33.26624989076275
+            ],
+            [
+              -116.56494140625001,
+              34.92197103616377
+            ],
+            [
+              -119.46533203125,
+              34.92197103616377
+            ],
+            [
+              -119.46533203125,
+              33.26624989076275
+            ]
+          ]
+        ]
+      }
+
+        self.temporal = ("2012-06-01", "2012-06-30")
         self.properties = {'eo:instrument': {'eq': 'Leica ADS100'}}
         self.limit = 20
