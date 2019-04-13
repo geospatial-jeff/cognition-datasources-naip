bafa047c9e71052687c063d7f696c0e8ff11cd80
Jeff Albrecht <geospatialjeff@gmail.com>
2019-04-12 20:27:35 -0600

    updating driver package

--- bin/driver-package.sh
+++ bin/driver-package.sh
@@ -24,16 +24,7 @@ rsync -ax $PYPATH/ $DEPLOY_DIR/python/ ${EXCLUDES[@]}
 mkdir -p $DEPLOY_DIR/python/datasources/sources/
 cp $DRIVERNAME.py $DEPLOY_DIR/python/datasources/sources/
 
-# Check for spatial index, if present add to share
-if [ -f "index.dat" ] && [ -f "index.idx" ]; then
-    mkdir $DEPLOY_DIR/share
-    cp index.dat $DEPLOY_DIR/share/
-    cp index.idx $DEPLOY_DIR/share/
-fi
 
 # Make lambda layer
 cd $DEPLOY_DIR
 zip -ruq ../lambda-layer.zip ./
-
-# Make lambda deployment package
-(cd .. && zip -q lambda-deploy.zip handler.py)
